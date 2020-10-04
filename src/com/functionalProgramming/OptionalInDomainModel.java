9d51dd7f855f35c981d1381822b727d7db2554c4
JaxonTeller <nishuzephyrus@outlook.com>
2020-10-04 07:55:09 +0530

    Using Maps and FlatMaps with Optional

--- src/com/functionalProgramming/OptionalInDomainModel.java
+++ src/com/functionalProgramming/OptionalInDomainModel.java
@@ -52,14 +52,28 @@ public class OptionalInDomainModel {
 
         Person person1=new Person(Optional.empty());
         getCarInsuranceName(person1);
+
+        Person person2=null;
+        getCarInsuranceNameWithOptionalMap(person2);
     }
 
     public static void getCarInsuranceName(Person person)
     {
+        /*If present or else was introduces in Java 9*/
         person.getCar()
                 .ifPresentOrElse(car -> car.getInsurance().
                                 ifPresentOrElse(insurance -> System.out.println(insurance.getInsuranceName()),
                         ()-> System.out.println("No car insurance name")),
                         ()-> System.out.println("Person doesn't have a car"));
+    }
+
+    public static void getCarInsuranceNameWithOptionalMap(Person person)
+    {
+          Optional<String> insuranceName=Optional.empty();
+          /*If person is null then it will throw Optional.of(person) will throw Null Pointer Exception
+          * hence using Optional.ofNullable*/
+          insuranceName=
+                  Optional.ofNullable(person).flatMap(Person::getCar).flatMap(Car::getInsurance).map(insurance -> insurance.getInsuranceName());
+         System.out.println(insuranceName.orElse("No insurance found"));
     }
 }
